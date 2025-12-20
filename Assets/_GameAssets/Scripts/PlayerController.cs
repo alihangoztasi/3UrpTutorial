c1529dbb597bbb2746571d282ce421fb2b2685fe
alihangoztasi <122389284+alihangoztasi@users.noreply.github.com>
2025-12-20 18:06:52 +0300

    v0.3b Character Controller Added

--- Assets/_GameAssets/Scripts/PlayerController.cs
+++ Assets/_GameAssets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     [SerializeField] private Transform _orientationTransform;
     
     [Header("Movement Settings")]
+    [SerializeField] private KeyCode _movementKey;
     [SerializeField] private float _movementSpeed;
 
     [Header("Jump Settings")]
@@ -16,9 +17,18 @@ public class NewMonoBehaviourScript : MonoBehaviour
     [SerializeField] private float _jumpCoolDown;
     [SerializeField]private bool _canJump;
 
+    [Header("Sliding Settings")]
+    [SerializeField] private KeyCode _slideKey;
+    [SerializeField] private float _slideMultiplier;
+    [SerializeField] private float _slideDrag;
+
+    private bool _isSliding;
+
+
     [Header("Ground Check Settings")]
     [SerializeField] private float _playerHeight;
     [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _groundDrag;
 
 
     private Rigidbody _playerRigidbody;
@@ -38,6 +48,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
     private void Update()
     {
         SetInputs();
+        SetPlayerDrag();
+        LimitPlayerSpeed();
     }
 
     private void FixedUpdate()
@@ -50,7 +62,18 @@ public class NewMonoBehaviourScript : MonoBehaviour
         _horizontalInput= Input.GetAxisRaw("Horizontal");
         _verticalInput= Input.GetAxisRaw("Vertical");
 
-        if (Input.GetKey(_jumpKey) && _canJump && IsGrounded())
+        if (Input.GetKeyDown(_slideKey))
+        {
+            _isSliding=true;
+            Debug.Log("Sliding");
+        }
+        else if(Input.GetKeyDown(_movementKey)){
+            _isSliding=false;
+            Debug.Log("player moving");
+        }
+
+
+        else if (Input.GetKey(_jumpKey) && _canJump && IsGrounded())
         {
             //Ziplama Islemi Gerceklesecek
             _canJump = false;
@@ -64,8 +87,35 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         _movementDirection = _orientationTransform.forward * _verticalInput 
         + _orientationTransform.right * _horizontalInput;
+        if(_isSliding) {
+        _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed * _slideMultiplier, ForceMode.Force);
+    }
+        else
+        {
+            _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force);
+        }
+    }
 
-        _playerRigidbody.AddForce(_movementDirection.normalized * _movementSpeed, ForceMode.Force);
+    private void SetPlayerDrag()
+    {
+        if(_isSliding){
+        _playerRigidbody.linearDamping = _slideDrag;
+        }
+        else
+        {
+             _playerRigidbody.linearDamping = _groundDrag;
+        }
+    }
+
+    private void LimitPlayerSpeed()
+    {
+        Vector3 flatVelocity = new Vector3(_playerRigidbody.linearVelocity.x, 0f, _playerRigidbody.linearVelocity.z);
+
+        if (flatVelocity.magnitude > _movementSpeed)
+        {
+            Vector3 LimitedVelocity = flatVelocity.normalized * _movementSpeed;
+            _playerRigidbody.linearVelocity = new Vector3(LimitedVelocity.x, _playerRigidbody.linearVelocity.y, LimitedVelocity.z);
+        }
     }
 
     private void SetPlayerJumping()
